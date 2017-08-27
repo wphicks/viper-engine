e6a0bf4d58ba6d1c6c1b1ae01fff439f58945bb0
William Hicks <william.p.hicks@gmail.com>
2017-08-27 14:58:26 -0400

    Add threaded timestamp testing of vqueue

--- test/vqueue_test.cpp
+++ test/vqueue_test.cpp
@@ -1,8 +1,11 @@
-#include <stdio>
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <thread>
 #include <chrono>
 #include <gtest/gtest.h>
 
-typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::steady_clock Clock;
 
 extern "C" {
   #include "vqueue.h"
@@ -11,63 +14,66 @@ extern "C" {
 /*Struct for items pushed to the queue with high resolution timestamp*/
 typedef struct {
   int data;
-  std::chrono::time_point timestamp;
+  Clock::time_point timestamp;
 } QItem;
 
-/*Task for producer threads*/
-void push_data(vqueue_t * queue) {
-  QItem * cur_item;
-  for (int i = 0; i < PUSHCOUNT; ++i) {
-    cur_item = malloc(sizeof(QItem));
-    cur_item->data = i;
-    cur_item->timestamp = Clock::now();
-    vqueue_push(*queue, ((void *) cur_item));
-  }
-}
-
-/*Task for consumer threads*/
-void read_data(vqueue_t * queue) {
-  void * retrieved;
-  QItem * cur_item;
-  int consumed_count = 0;
-  auto prev_timestamp = std::chrono::time_point::min();
-  int prev_data = -1;
-  while (consumed_count < PUSHCOUNT * PRODUCERTHREADS / CONSUMERTHREADS) {
-    if (vqueue_pop(*queue, &retrieved)) {
-      ++consumed_count;
-      cur_item = (QItem *) retrieved;
-      if (cur_item->timestamp < prev_timestamp) {
-        std::cout << std::chrono::duration_cat<std::chrono::nanoseconds>(
-            cur_item->timestamp - prev_timestamp
-        );
-      }
-      prev_timestamp = cur_item->timestamp;
-      prev_data = cur_item->data;
-    }
-  }
-}
 
 class vqueueTest : public ::testing::Test {
  protected:
   const int k_max_queue_size = 11;
-  const int threaded_push_count = 512;
-  const int consumer_thread_count = 1;
-  const int producer_thread_count = 2; // Make this int multiple of consumers
+  static const int threaded_push_count = 512;
+  static const int consumer_thread_count = 2;
+  static const int producer_thread_count = 4;
   void * queue_buffer;
   vqueue_t queue;
   int data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
   void * retrieved;
-  int previous;
-  int next;
+  std::vector< std::shared_ptr<std::thread> > consumers;
+  std::vector< std::shared_ptr<std::thread> > producers;
 
   virtual void SetUp() {
     queue_buffer = malloc(vqueue_get_bytes_required(k_max_queue_size));
     queue = vqueue_create(queue_buffer, k_max_queue_size);
-    previous = 0;
   }
 
   virtual void TearDown() {
     free(queue_buffer);
+  }
+
+  /*Task for producer threads*/
+  static void push_data(vqueue_t * queue) {
+    QItem * cur_item;
+    for (int i = 0; i < threaded_push_count; ++i) {
+      cur_item = (QItem *) malloc(sizeof(QItem *));
+      cur_item->data = i;
+      cur_item->timestamp = Clock::now();
+      vqueue_push(*queue, ((void *) cur_item));
+    }
+  }
+
+  /*Task for consumer threads*/
+  static void read_data(vqueue_t * queue) {
+    void * retrieved;
+    QItem * cur_item;
+    int consumed_count = 0;
+    auto prev_timestamp = Clock::time_point(std::chrono::nanoseconds::min());
+    int prev_data = -1;
+    int error_count = 0;
+    while (
+        consumed_count <
+        threaded_push_count * producer_thread_count / consumer_thread_count
+        ) {
+      if (vqueue_pop(*queue, &retrieved)) {
+        ++consumed_count;
+        cur_item = (QItem *) retrieved;
+        if (cur_item->timestamp < prev_timestamp) {
+          ++error_count;
+        }
+        prev_timestamp = cur_item->timestamp;
+        prev_data = cur_item->data;
+      }
+    }
+    EXPECT_GT(consumed_count / 100, error_count);
   }
 };
 
@@ -82,6 +88,21 @@ TEST_F(vqueueTest, PushPop) {
 }
 
 TEST_F(vqueueTest, ThreadedPushPop) {
+  for (int i = 0; i < producer_thread_count; ++i) {
+    producers.push_back(std::make_shared<std::thread>(push_data, &queue));
+  }
+  std::cout << "Producers created\n";
+  for (int i = 0; i < consumer_thread_count; ++i) {
+    consumers.push_back(std::make_shared<std::thread>(read_data, &queue));
+  }
+  /*read_data(&queue);*/
+  std::cout << "Consumers created\n";
+  for (auto cur_thread : producers) {
+    cur_thread->detach();
+  }
+  for (auto cur_thread : consumers) {
+    cur_thread->join();
+  }
 }
 
 int main(int argc, char **argv) {
